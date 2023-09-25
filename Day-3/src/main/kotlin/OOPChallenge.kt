6574d7d4d6ac7860cebf68dec089a021368fc09d
Moin Khan <moin6k@gmail.com>
2023-09-26 00:11:31 +0530

    Day-3

--- Day-3/src/main/kotlin/OOPChallenge.kt
+++ Day-3/src/main/kotlin/OOPChallenge.kt
@@ -2,29 +2,33 @@ fun main(){
     val person1 = Bank("Moin")
     person1.deposit(5000)
     person1.withdraw(1000)
+    person1.deposit(12)
+    person1.withdraw(1000)
     person1.getListOfTransactions()
+    person1.accountBalance()
 }
 
 class Bank(var accountName:String){
     var balance = 0
     var transactions = mutableListOf<String>()
 
-    fun deposit(depositAmount:Int):String{
+    fun deposit(depositAmount:Int){
         if (depositAmount > 0) {
             this.balance = this.balance + depositAmount
+            transactions.add("Deposited $depositAmount")
+            println("Deposited ${depositAmount} Successfully!")
+        }else{
+            println("Deposit Amount Should be not more than 0")
         }
-        transactions.add("Deposited $depositAmount")
-        return "Deposited ${depositAmount} Successfully!"
     }
 
-    fun withdraw(withdrawAmount: Int):String{
+    fun withdraw(withdrawAmount: Int){
         if (withdrawAmount <= this.balance){
             this.balance = this.balance - withdrawAmount
             transactions.add("Withdrawl ${withdrawAmount}")
-            return "Withdrawn ${withdrawAmount} Successfully!"
+            println("Withdrawn ${withdrawAmount} Successfully!")
         }else{
-            println("Insufficient Balance!")
-            return ""
+            println("Insufficient Balance!,Your Balance is ${this.balance}")
         }
     }
 
